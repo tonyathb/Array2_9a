4cfa2dd49d82471235059256435f659abc22817e
tonyathb <tonya_thb@abv.bg>
2024-02-16 13:18:07 +0200

    new project MiddleNumbers

---
+++ MiddleNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MiddleNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers= Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int middle = numbers.Length / 2;
+            if ( numbers.Length == 1)
+            {
+                Console.WriteLine(numbers[0]);
+            }
+            else if ( numbers.Length %2== 0)
+            {
+                Console.WriteLine(numbers[middle-1]);
+                Console.WriteLine(numbers[middle]);
+            }
+            else
+            {
+
+                Console.WriteLine(numbers[middle - 1]);
+                Console.WriteLine(numbers[middle]);
+                Console.WriteLine(numbers[middle + 1]);
+            }
+        }
+    }
+}
